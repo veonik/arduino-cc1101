ef7b059b2ca3ba51fcb347c307691523169e5ec3
hazza26 <33527565+hazza26@users.noreply.github.com>
2020-10-27 19:00:12 +0000

    Update cc1101.h

--- src/cc1101.h
+++ src/cc1101.h
@@ -234,8 +234,8 @@ enum RFSTATE
 #define CC1101_DEFVAL_FREQ0_868  0x76        // Frequency Control Word, Low Byte
 // Carrier frequency = 900 MHz
 #define CC1101_DEFVAL_FREQ2_900  0x22        // Frequency Control Word, High Byte
-#define CC1101_DEFVAL_FREQ1_900  0x9D        // Frequency Control Word, Middle Byte
-#define CC1101_DEFVAL_FREQ0_900  0x88        // Frequency Control Word, Low Byte
+#define CC1101_DEFVAL_FREQ1_900  0x9E        // Frequency Control Word, Middle Byte
+#define CC1101_DEFVAL_FREQ0_900  0x84        // Frequency Control Word, Low Byte
 // Carrier frequency = 918 MHz
 #define CC1101_DEFVAL_FREQ2_918  0x23        // Frequency Control Word, High Byte
 #define CC1101_DEFVAL_FREQ1_918  0x4E        // Frequency Control Word, Middle Byte
